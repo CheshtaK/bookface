70d376c876f97568393f11b8c9542381d062416d
abhinavsri360 <abhinav.022000@gmail.com>
2020-06-20 18:29:31 +0530

    more genres added

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -8,9 +8,24 @@ class Images {
   static const String art_and_music = 'assets/art_and_music.jpg';
   static const String comic = 'assets/comic.jpg';
   static const String computer_and_tech = 'assets/computer_and_tech.jpg';
-  static const String cooking = 'assets/cooking.jpg';
   static const String health_and_fitness = 'assets/health_and_fitness.jpg';
   static const String romance = 'assets/romance.jpg';
   static const String mystery = 'assets/mystery.jpg';
   static const String travel = 'assets/travel.jpg';
+  static const String action_and_adventure = 'assets/action_and_adventure.jpg';
+  static const String biography_and_autobiography =
+      'assets/biography_and_autobiography.jpg';
+  static const String classics = 'assets/classics.jpg';
+  static const String cookbooks = 'assets/cookbooks.jpg';
+  static const String fantasy = 'assets/fantasy.jpg';
+  static const String historical_fiction = 'assets/historical_fiction.jpg';
+  static const String history = 'assets/history.jpg';
+  static const String horror = 'assets/horror.jpg';
+  static const String literary = 'assets/literary.jpg';
+  static const String memoir = 'assets/memoir.jpg';
+  static const String self_help = 'assets/self_help.png';
+  static const String suspense_and_thriller =
+      'assets/suspense_and_thriller.jpg';
+  static const String true_crime = 'assets/true_crime.jpg';
+  static const String women_fiction = 'assets/women_fiction.png';
 }
