cdef3ef694b6e67e6e3d7468a95d30ba80b00436
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-23 19:10:20 +0530

    Implemented Google Sign In. Fixes #1 (#19)

--- lib/login.dart
+++ lib/login.dart
@@ -2,8 +2,64 @@ import 'package:bookface/constants/strings.dart';
 import 'package:bookface/routes/routes.dart';
 import 'package:bookface/widgets/custom_outline_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginPage extends StatelessWidget {
+final GoogleSignIn googleUser = GoogleSignIn();
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  
+  /* This boolean accounts for the state if a user has been logged in or not, might come in handy later while conditionally rendering something in the future. */
+  bool isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /*
+      This method once initalized will listen for all the events related to googleAccount sign in, logout etc. and call the control method each time something happens.
+    */
+
+    googleUser.onCurrentUserChanged.listen((googleAccount) {
+      controlLogin(googleAccount);
+    }, onError: (loginError) {
+      print("GOOGLE LOGIN ERROR : " + loginError.toString());
+    });
+    
+    /*
+
+      THIS BELOW METHOD WILL CHECK IF A USER HAS ALREADY BEEN LOGGED IN TO THE APP BEFORE, AND AUTOMATICALLY SKIP THE LOGIN SCREEN.
+
+      If a PlatformException(sign_in_required, com.google.android.gms.common.api.ApiException: 4: 4: , null)) occurs, please uncheck 'Uncaught Exceptions in the Breakpoints section of the Debug SidePanel, if you're using VSCode.
+
+      This is a known issue in google_sign_in package, if no logged in user account is found. More on that here : 
+      https://github.com/flutter/flutter/issues/36041
+    */
+
+    googleUser.signInSilently(suppressErrors: true).then((googleAccount) {
+      controlLogin(googleAccount);
+    }).catchError((e) => print("AUTOMATIC LOGIN ERROR : " + e.toString()));
+  }
+
+  /* This below method acts as a control for every googleAccount login and will push a new route on the context. */
+
+  void controlLogin(GoogleSignInAccount googleAccount) async {
+    if (googleAccount != null) {
+      setState(() {
+        isSignedIn = true;
+      });
+      print(googleAccount.displayName + " Just Logged In");
+      Navigator.of(context).pushNamed(Routes.category);
+    } else {
+      isSignedIn = false;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +98,7 @@ class LoginPage extends StatelessWidget {
                 width: 300.0,
                 text: Strings.google_login,
                 color: Colors.white70,
-                onPressed: () =>
-                    Navigator.of(context).pushNamed(Routes.category),
+                onPressed: () => googleUser.signIn(),
               ),
               Padding(
                 padding: EdgeInsets.only(bottom: 5),
